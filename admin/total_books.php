79f624f9b649f75dbcffcdec2966a759281d1a73
Jundel-Malazarte <jundelmalazarte348@gmail.com>
2025-03-19 10:58:51 +0800

    Adding bootstrap in total books page

--- admin/total_books.php
+++ admin/total_books.php
@@ -69,7 +69,7 @@ $paginated_books = array_slice($books, $start, $items_per_page);
     <title>Total Books</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">  
     <link rel="icon" href="./images/Readscape.png">
     <style>
         body {
